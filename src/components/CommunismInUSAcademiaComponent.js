e9b5b96a24eabd6f1f3b8a827fd07de99cd6806a
Jade Do <ctd45@miami.edu>
2018-10-24 21:17:38 -0400

    Functional Components 10/24

--- src/components/CommunismInUSAcademiaComponent.js
+++ src/components/CommunismInUSAcademiaComponent.js
@@ -1,76 +1,33 @@
 import React, {Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Media, Button } from 'reactstrap';
 
-class CommunismInUSAcademia extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedArticle: null
-        };
-    }
-
-/*     onArticleSelect(article){
-        this.setState( {selectedArticle: article });
-    }
-
-    renderArticle(article){
-        if (article != null){
-            return (
-                <div key={article.id} className="col-12 mt-5">
-                    <Media heading> {article.label} </Media>
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={article.image} alt={article.title} height="650px" width="450px"/>
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{article.title}</Media>
-                            <Media heading><em>- {article.author}</em></Media>
-                            <p>"{article.abstract}"</p>
-                            <p><em>- {article.abstract_author}</em></p>
-                            <Button size="large" color="danger" target="_blank" href={article.link}> Read More </Button>
-                        </Media>
-                    </Media>
-                </div>
-            )
-        } else {
-            return (
-                <div></div>
-            )
-        }
-    }
-
-    render() {
-        const newsList = this.props.articles.map((article) =>  {
-            return (
-                <div className="col-12 col-md-3 m-1">
-                     <Card key={article.id}
-                        onClick={() =>this.onArticleSelect(article)}>
-                        <CardTitle>{article.title}</CardTitle>
-                        <CardImg height="" src={article.image} alt={article.name}/>  
-                        <CardBody><span class="fa-circle"></span></CardBody>
-                    </Card> 
-                </div>
-            )
-        });
-
+    function RenderArticleItem({article}) {
         return (
-            <div className="container">
-                <div className="row">
-                    {newsList}
-                </div>
-                <div className="row">
-                    <div className="col-12">
-                        {this.renderArticle(this.state.selectedArticle)}
-                    </div>
-                </div>
+            <div key={article.id} className="col-12 mt-5">
+                <Media heading> {article.label} </Media>
+                <Media tag="li">
+                    <Media left middle>
+                        <Card>
+                            <CardImg src={article.image} alt={article.title} h-650 w-50></CardImg>
+                            <CardImgOverlay><Button size="large" color="primary" block target="_blank" href={article.link}> Read More </Button></CardImgOverlay>
+                        </Card>
+                        {/* <Media object src={article.image} alt={article.title} height="650px" width="450px"/> */}
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{article.title}</Media>
+                        <Media heading><em>- {article.author}</em></Media>
+                        <p>"{article.abstract}"</p>
+                        <p><em>- {article.abstract_author}</em></p>
+                        {/* <Button size="large" color="danger" target="_blank" href={article.link}> Read More </Button> */}
+                        <div>
+                            <RenderComments commentsParam={article.comments}></RenderComments>
+                        </div>
+                    </Media>
+                </Media>
             </div>
-        );
+        )};
 
-    }
- */
-
-    renderComments(commentsParam){
+    function RenderComments({commentsParam}){
         if (commentsParam != null){
             const theComments = commentsParam.map((oneComment) => {
                 return(
@@ -101,31 +58,10 @@ class CommunismInUSAcademia extends Component {
     }
 
 
-    render() {
-        const news_list = this.props.articles.map((article) => {
+    const CommunismInUSAcademia = (props) => {
+        const news_list = props.articles.map((article) => {
             return (
-                <div key={article.id} className="col-12 mt-5">
-                    <Media heading> {article.label} </Media>
-                    <Media tag="li">
-                        <Media left middle>
-                            <Card>
-                                <CardImg src={article.image} alt={article.title} h-650 w-50></CardImg>
-                                <CardImgOverlay><Button size="large" color="primary" block target="_blank" href={article.link}> Read More </Button></CardImgOverlay>
-                            </Card>
-                            {/* <Media object src={article.image} alt={article.title} height="650px" width="450px"/> */}
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{article.title}</Media>
-                            <Media heading><em>- {article.author}</em></Media>
-                            <p>"{article.abstract}"</p>
-                            <p><em>- {article.abstract_author}</em></p>
-                            {/* <Button size="large" color="danger" target="_blank" href={article.link}> Read More </Button> */}
-                            <div>
-                                {this.renderComments(article.comments)}
-                            </div>
-                        </Media>
-                    </Media>
-                </div>
+                <RenderArticleItem article={article}></RenderArticleItem>
             )
         });
 
@@ -140,6 +76,6 @@ class CommunismInUSAcademia extends Component {
         )
     }
 
-}
+
 
 export default CommunismInUSAcademia;
